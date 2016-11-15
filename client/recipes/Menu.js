fd1925f1862453ead4c0297e5f7be49d7c4ef9a2
Ty Short <covertsonic@gmail.com>
2016-11-15 15:15:11 -0500

    Menu buildout started

--- client/recipes/Menu.js
+++ client/recipes/Menu.js
@@ -2,14 +2,14 @@
 
 //Meteor.subscribe('recipes'); //don't want to pull all of recipes for performative reasons.
 
-Template.Menu.onCreated(function () {
+Template.MenuItem.onCreated(function () {
     let self = this;
     self.autorun(function () { //unsubscribe from old subscriptions
         self.subscribe('recipes');
     })
 });
 
-Template.Menu.helpers({
+Template.MenuItem.helpers({
     recipes: () => {
         xyz = Recipes.find({
             inMenu: true

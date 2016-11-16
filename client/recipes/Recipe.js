d1017c0c47def66b048ef36a2a5457128958e466
Ty Short <covertsonic@gmail.com>
2016-11-16 10:47:21 -0500

    Thru end of video 22 updating with a quickform
    Signed-off-by: Ty Short <covertsonic@gmail.com>

--- client/recipes/Recipe.js
+++ client/recipes/Recipe.js
@@ -1,3 +1,9 @@
+Template.Recipe.helpers({
+    updateRecipeId: function() { //using a "real function instead of ()=> since that will mess up the "this" variable
+        return this._id;
+    }
+});
+
 Template.Recipe.events({
     'click .toggle-menu': function () {
         Meteor.call('toggleMenuItem', this._id, this.inMenu);

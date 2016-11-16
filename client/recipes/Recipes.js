2c52b59c44fcf085216146e8d664762bb8a70cb0
Ty Short <covertsonic@gmail.com>
2016-11-16 15:47:53 -0500

    Thru end of video 25 wrapping up
    Signed-off-by: Ty Short <covertsonic@gmail.com>

--- client/recipes/Recipes.js
+++ client/recipes/Recipes.js
@@ -2,6 +2,13 @@
 
 //Meteor.subscribe('recipes'); //don't want to pull all of recipes for performative reasons.
 
+Template.Recipes.events({
+    'click .new-recipe' : function(){
+        Session.set('newRecipe', true);
+    }
+});
+
+
 Template.Recipes.onCreated(function () {
     let self = this;
     self.autorun(function () { //unsubscribe from old subscriptions

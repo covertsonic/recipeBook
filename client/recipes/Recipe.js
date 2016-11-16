57429c6c1b35322e52841436d2e9b5b23098baf2
Ty Short <covertsonic@gmail.com>
2016-11-16 14:59:34 -0500

    Thru end of video 24 scoped reactivity with reactive vars
    Signed-off-by: Ty Short <covertsonic@gmail.com>

--- client/recipes/Recipe.js
+++ client/recipes/Recipe.js
@@ -1,7 +1,14 @@
+Template.Recipe.onCreated(function(){
+    this.editMode = new ReactiveVar(false);
+})
+
 Template.Recipe.helpers({
     updateRecipeId: function() { //using a "real function instead of ()=> since that will mess up the "this" variable
         //console.log(this._id);
         return this._id;
+    },
+    editMode: function(){
+        return Template.instance().editMode.get(); //this instance
     }
 });
 
@@ -15,7 +22,8 @@ Template.Recipe.events({
         Meteor.call('deleteRecipe',this._id);
 
     },
-    'click .fa-pencil' : function(){ //again ()=> will make it so the this is not accessible.
-        Session.set('editMode',!Session.get('editMode'));
+    'click .fa-pencil' : function(event, template){ //again ()=> will make it so the this is not accessible.
+        //Session.set('editMode',!Session.get('editMode')); //removed in favor of the ReactiveVar
+        template.editMode.set(!template.editMode.get());
     }
 });
--- readme.txt
+++ readme.txt
@@ -1,4 +1,9 @@
-When downloading for the first time, you'll need to run the npm install so that the node_modules folder becomes populated.
+Application uses the Meteor stack.
+
+Great tutorial from leveluptuts
+https://www.google.com/search?q=leveluptuts+intermediate+meteor&rlz=1C1CHFX_enUS707US707&oq=leveluptuts+intermediate+meteor&aqs=chrome..69i57.4478j0j4&sourceid=chrome&ie=UTF-8
+
+When running this meteor project for the first time, you'll need to run the npm install so that the node_modules folder becomes populated.
 $ meteor npm install
 
 Then, create your own settings.json based on the settings.json example.
